87e412b63bc1a9657246c4afaf49043e54184421
Priyanshu Dwivedi <Priyanshu.Dwivedi@lntinfotech.com>
2023-03-02 23:55:21 +0530

    cppSol

---
+++ cppSolution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int rec(int idx,bool fg,vector<int>& pr,vector<vector<int> >& dp){
+        if(idx==pr.size()){
+            return 0;
+        }
+        
+        if(dp[idx][fg]!=-1){
+            return dp[idx][fg];
+        }
+        
+        if(!fg){
+            int opt = rec(idx+1,1,pr,dp) - pr[idx];
+            int opt2 = rec(idx+1,0,pr,dp);
+            return dp[idx][fg] = max(opt,opt2);
+        }
+        else{
+            int opt = pr[idx];
+            int opt2 = rec(idx+1,1,pr,dp);
+            return dp[idx][fg] = max(opt,opt2);
+        }
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int> > dp(n,vector<int>(2,-1));
+        
+        int ans = rec(0,0,prices,dp);
+        
+        return ans;
+    }
+};
